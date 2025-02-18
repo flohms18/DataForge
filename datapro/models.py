8ef517a9046efe70a708323e78ba5772a06e3412
flotr18 <trouilletflorent@outlook.com>
2025-02-18 20:06:42 +0100

    Update models.py

--- datapro/models.py
+++ datapro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class DataProfession(models.Model):
+    DataProName = models.CharField(max_length=200)
+    DataProDescription = models.CharField(max_length=600)
+    DataProTools = models.CharField(max_length=200)
