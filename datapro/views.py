861e7115fe2ec88fa74ba74f8f1fe467a3f9ebf7
flotr18 <trouilletflorent@outlook.com>
2025-02-19 12:23:43 +0100

    Add detail page for datapro

--- datapro/views.py
+++ datapro/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the datapro index.")
+
+def DataProDetail(request,DataProfession_id):
+    return HttpResponse("Hello, world. You're at the datapro page.")
