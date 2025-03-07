c7f97d24bfab7c2572554674da1cdea74f17cda6
flotr18 <trouilletflorent@outlook.com>
2025-03-07 11:22:36 +0100

    Add cards professions

--- datapro/views.py
+++ datapro/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from .models import DataCareer
 
 
-def index(request):
-    return render(request, "datapro/index.html")
+def career(request):
+    queryset = DataCareer.objects.all()  
+    return render(request, "datapro/career.html",{'queryset': queryset})
 
 def about(request):
     return render(request,'datapro/about.html')
 
-def career(request):
-    return render(request,'datapro/career.html')
+def index(request):
+    return render(request,'datapro/index.html')
